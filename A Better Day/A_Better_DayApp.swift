cd4837bfa90f71ef36086ae701358c10ea096748
jcalvillo31 <josecalvillo31@gmail.com>
2025-06-13 21:09:10 -0400

    Initial Commit

---
+++ A Better Day/A_Better_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Better_DayApp.swift
+//  A Better Day
+//
+//  Created by Jose Calvillo on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct A_Better_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Better DayTests/A_Better_DayTests.swift
@@ -0,0 +1,17 @@
+//
+//  A_Better_DayTests.swift
+//  A Better DayTests
+//
+//  Created by Jose Calvillo on 6/13/25.
+//
+
+import Testing
+@testable import A_Better_Day
+
+struct A_Better_DayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
